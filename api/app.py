b5c499329f327c7d9503d64c3030a7aed2762ae6
Tsotetsi <me@tsotetsithapelo.co.za>
2022-09-18 14:53:29 +0200

    Refactor. Return/store system_file_name instead of the actual file_name

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
-from crypt import methods
+from genericpath import exists
 import os
 import sqlite3
+import uuid
 
 from flask import Flask, send_from_directory, request, render_template, flash
 from flask_uploads import DOCUMENTS, UploadSet, configure_uploads
@@ -10,9 +11,9 @@ app = Flask(__name__)
 CORS(app)
 
 documents = UploadSet("documents", DOCUMENTS)
-app.config["UPLOADED_DOCUMENTS_DEST"] = "static/documents"
+app.config["UPLOADED_DOCUMENTS_DEST"] = "upload_documents/resume"
 app.config["SECRET_KEY"] = os.urandom(24)
-app.config["ALLOWED_EXTENSIONS"] = ["docx", "doc", "pdf"]
+app.config["ALLOWED_EXTENSIONS"] = [".docx", ".doc", ".pdf"]
 app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024  # 10mb
 configure_uploads(app, documents)
 
@@ -28,8 +29,23 @@ def main():
 
 @app.route("/media-upload", methods=["GET", "POST"])
 def media_upload():
+
     if request.method == "POST" and 'document' in request.files:
-        documents.save(request.files['document'])
+
+        system_name = uuid.uuid4().hex + '.pdf'
+        original_name = request.files['document'].filename
+
+        documents.save(request.files['document'],  name=system_name)
+
+        connection = get_db_connection()
+        cur = connection.cursor()
+
+        cur.execute("INSERT INTO documents (original_name, system_name) VALUES (?, ?)",
+                    (original_name, system_name)
+                    )
+        connection.commit()
+        connection.close()
+
         flash("Document uploaded successfully.")
         return render_template('media-upload-success.html')
     return render_template('media-upload.html')
@@ -41,11 +57,12 @@ def media_upload_view():
     conn.close()
     return render_template('media-view.html', docs=docs)
 
-@app.route("/media-upload-download/<file_name>")
-def media_upload_download(file_name):
-    working_dir = os.path.abspath(os.getcwd())
-    file_path = working_dir + '/static/'
-    return send_from_directory(file_path, file_name)
+@app.route("/media-upload-download/<system_file_name>")
+def media_upload_download(system_file_name):
+    file_path = os.path.abspath(os.getcwd()) + '/upload_documents/resume/'
+    if exists(file_path):
+        return send_from_directory(file_path, system_file_name)
+
 
 if __name__ == "__main__":
     app.run()
